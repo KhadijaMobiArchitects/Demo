9deae4109607bdf4e44dcfdcd542ab00a99e7dd9
KhadijaMobiArchitects <100199763+KhadijaMobiArchitects@users.noreply.github.com>
2022-06-22 01:10:55 +0100

    Delete src directory

--- src/api/httpComm.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'http://rh-api-dev-mobiarchitects.azurewebsites.net',
-});
--- src/utils/getJwToken.js
+++
@@ -1,6 +0,0 @@
-export const getJwToken = () => {
-  const user = JSON.parse(localStorage.getItem('userData'));
-  if (user && user.jwToken) {
-    return { Authorization: 'Bearer ' + user.jwToken };
-  }
-};
